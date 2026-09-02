36e53379656e90a86dac180813d1e53e68cb7bfc
Pavandev20 <pavan.rajput@mindcrew.in>
2026-09-02 12:50:03 +0530

    update profile frontend changes

---
+++ frontend/src/types/mui-icons.d.ts
@@ -0,0 +1,6 @@
+declare module '@mui/icons-material/esm/*' {
+  import { SvgIconProps } from '@mui/material/SvgIcon';
+  import { ComponentType } from 'react';
+  const content: ComponentType<SvgIconProps>;
+  export default content;
+}
